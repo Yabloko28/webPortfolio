b31007d8aedba201df2dd39c1b117d8c046f4bee
nadyaPeeps <nadezhda.fed.28@gmail.com>
2016-06-01 22:38:12 -0400

    adding menu and creating a contact form

--- js/sketch.js
+++ js/sketch.js
@@ -19,20 +19,20 @@ function setup() {
 
 function draw() {
 background(252, 171, 98);
-//   noFill();
-//   for (var j = -100; height+100 > j; j+=ystep) {
-//     beginShape();
-//     vertex(-100, j);
-//     for (var i = -100; i < width+100; i+=xstep) {
-//       nx = i/234;
-//       ny = j/165;
-//       var v = noise(nx, ny, nz);
-//       y = map(v, 0, 1, -100, 100)+j;
-//       curveVertex(i, y);
-//       //println(v);
-//     }
-//     vertex(width, j);
-//     endShape();
-// }
-//     nz+=.01;
+  noFill();
+  for (var j = -100; height+100 > j; j+=ystep) {
+    beginShape();
+    vertex(-100, j);
+    for (var i = -100; i < width+100; i+=xstep) {
+      nx = i/234;
+      ny = j/165;
+      var v = noise(nx, ny, nz);
+      y = map(v, 0, 1, -100, 100)+j;
+      curveVertex(i, y);
+      //println(v);
+    }
+    vertex(width, j);
+    endShape();
+}
+    nz+=.01;
 }
