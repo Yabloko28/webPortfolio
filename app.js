28ae2cb6b802d739f29a13b6c7ed507397eb7191
nadyaPeeps <nadezhda.fed.28@gmail.com>
2016-06-02 20:20:26 -0400

    adding content

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ $(document).ready(function(e) {
 			output += '<div class="project-info">' + '<h3>' + val.title + '</h3>';
 			output += '<p class="tools">' + val.tools + '</p>';
 			output += '<p class="description">' + val.description + '</p>';
-			output += '<a class="button" href="' + val.button1Href + '" id="' + val.button1ID + '">' + val.button1 + '</a>',
-			output += '<a class="button" href="' + val.button2Href + '">' + val.button2 + '</a>',
-			output += '</div>' + '</li>'
+			output += '<a class="button" href="' + val.button1Href + '" id="' + val.button1ID + '"';
+			if (val.button1ID != "readMore") {
+				output += ' target="_blank" ';
+			};
+			output += '>' + val.button1 + '</a>';
+			output += '<a target="_blank" class="button" href="' + val.button2Href + '">' + val.button2 + '</a>';
+			output += '</div>' + '</li>';
 		})
 		$('#works').html(output);
 	// 	$('#works').find('#readMore').on('click', function(e) {
