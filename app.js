60c1a53d05f55c6ede4eb7d3796bd14dd1c1e2ca
nadyaPeeps <nadezhda.fed.28@gmail.com>
2016-06-03 22:27:41 -0400

    contact form with ajax and php

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function(e) {
 	$.getJSON('data.json', function(data, e) {
 		var output = '';
 		$.each(data, function(key, val) {
-			output += '<li>' + '<div class="project-image">' + '<img src="' + val.image + '">' + '</div>';
+			output += '<li>' + '<div class="project-image">' + '<img class="desktop" src="' + val.image + '">' + '<img class="mobile" src="' + val.imageMobile + '">' + '</div>';
 			output += '<div class="project-info">' + '<h3>' + val.title + '</h3>';
 			output += '<p class="tools">' + val.tools + '</p>';
 			output += '<p class="description">' + val.description + '</p>';
@@ -29,7 +29,67 @@ $(document).ready(function(e) {
 	// 	$('#project-container').load('templates/maude.html');
 	// });
 	})
+	var menu = $('#sliding-menu')
+	var button = $('#menu-icon');
+	var open = false;
+	button.click(function() {
+	    if (open==false) {
+	    	menu.slideDown(400, function() {});
+	  		open = true;
+	   	} else {
+	   		menu.slideUp(400, function() {});
+	   		open = false;
+	   	}
+	})
 })
+
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+$(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+
+    // Serialize the form data.
+var formData = $(form).serialize();
+// Submit the form using AJAX.
+$.ajax({
+    type: 'POST',
+    url: $(form).attr('action'),
+    data: formData
+})
+.done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+    $(formMessages).removeClass('error');
+    $(formMessages).addClass('success');
+
+    // Set the message text.
+    $(formMessages).text(response);
+
+    // Clear the form.
+    $('#name').val('');
+    $('#email').val('');
+    $('#message').val('');
+})
+.fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+    } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+});
+});
+});
 
 //---------------------------------------------------------------------------
 
